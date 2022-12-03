dfe34f4f2cba7bac9a02d3dbdd6fec239739f8ca
Samuel Gélineau <gelisam+github@gmail.com>
2022-12-03 12:14:08 -0500

    calculate time worked today so far

---
+++ README.md
@@ -0,0 +1,26 @@
+# timesheet
+
+A simple program for calculating how many hours I have worked today so far.
+
+Here's an example input file:
+
+    $ cat example-timesheet.txt
+    Mon 2022-11-14
+      9:00-9:45 Emails
+      9:45-12:00 Implementing a timesheet program
+      13:15-16:00 Implementing a timesheet program
+      16:00-17:30 Testing the timesheet program 
+
+    Tue 2022-11-15
+      8:45-9:45 Emails
+      10:00- Documenting the timesheet program
+
+If I run `timesheet` at 10:00, then it will say that I have worked 1 hour today
+so far, namely from 8:45 to 9:45. Since the last range is open, if I run
+`timesheet` at 10:15, it will take that extra 15 minutes into account and say
+that I have worked 1 hour and 15 minutes today so far.
+
+    $ date +"%H:%M"
+    10:16
+    $ cabal run timesheet < example-timesheet.txt
+    I have worked 1 hours and 15 minutes today so far.
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,204 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, NamedFieldPuns, ViewPatterns #-}
 module Main where
 
+import Control.Category ((>>>))
+import Data.Maybe (fromMaybe)
+import Data.Time (DiffTime, LocalTime(LocalTime), TimeOfDay(TimeOfDay))
+import Text.Read (readMaybe)
+import qualified Data.Time as Time
+import qualified Data.Time.Calendar.OrdinalDate as Time
 
-main :: IO ()
+
+data Day = Day
+  { date
+      :: String
+  , lineItems
+      :: [LineItem]
+  }
+  deriving Show
+
+data LineItem = LineItem
+  { startTime
+      :: TimeOfDay
+  , stopTime
+      :: Maybe TimeOfDay
+  , description
+      :: String
+  }
+  deriving Show
+
+parseDay
+  :: [String]
+  -> Day
+parseDay
+  ( date_
+  : ( fmap parseLineItem
+   -> lineItems_
+    )
+  )
+  = Day date_ lineItems_
+parseDay ss
+  = error $ "missing date header in "
+         ++ show ss
+
+parseLineItem
+  :: String
+  -> LineItem
+parseLineItem
+  ( ' '
+  : ' '
+  : ( break (== '-')
+   -> ( parseTimestamp -> startTime_
+      , '-'
+      : ( break (== ' ')
+       -> ( parseMaybeTimestamp -> stopTime_
+          , ' ':description_
+          )
+        )
+      )
+    )
+  )
+  = LineItem startTime_ stopTime_ description_
+parseLineItem s
+  = error $ "missing indentation in "
+         ++ show s
+
+parseMaybeTimestamp
+  :: String
+  -> Maybe TimeOfDay
+parseMaybeTimestamp ""
+  = Nothing
+parseMaybeTimestamp s
+  = Just (parseTimestamp s)
+
+parseTimestamp
+  :: String
+  -> TimeOfDay
+parseTimestamp
+  ( break (== ':')
+ -> ( ( parseInt
+     -> hours
+      )
+    , ':'
+    : ( parseInt
+     -> minutes
+      )
+    )
+  )
+  = TimeOfDay hours minutes 0
+parseTimestamp s
+  = error $ "not a timestamp: "
+         ++ show s
+
+parseInt
+  :: String
+  -> Int
+parseInt s
+  = case readMaybe s of
+      Just n
+        -> n
+      Nothing
+        -> error $ "not an integer: "
+                ++ show s
+
+parseDays
+  :: [String]
+  -> [Day]
+parseDays = fmap parseDay . filter (not . null) . go []
+  where
+    go
+      :: [String]
+      -> [String]
+      -> [[String]]
+    go currentDay (s@(' ':_) : ss)
+      = go (currentDay ++ [s]) ss
+    go currentDay (s:ss)
+      = currentDay
+      : go [s] ss
+    go currentDay []
+      = [currentDay]
+
+
+lineItemDuration
+  :: TimeOfDay
+  -> LineItem
+  -> DiffTime
+lineItemDuration now (LineItem {startTime, stopTime})
+    = Time.timeOfDayToTime (fromMaybe now stopTime)
+    - Time.timeOfDayToTime startTime
+
+-- doesn't matter which day, as long as everything is on the same day
+timeOfDay_to_localTime
+  :: TimeOfDay
+  -> LocalTime
+timeOfDay_to_localTime
+  = LocalTime (Time.fromOrdinalDate 1970 0)
+
+
+minutesToDiffTime
+  :: Integer
+  -> DiffTime
+minutesToDiffTime minutes
+  = Time.secondsToDiffTime (minutes * 60)
+
+roundToNearestFifteenMinutes
+  :: DiffTime
+  -> DiffTime
+roundToNearestFifteenMinutes diffTime
+  = let diffToPico :: DiffTime -> Rational
+        diffToPico = fromIntegral . Time.diffTimeToPicoseconds
+        
+        picoToDiff :: Rational -> DiffTime
+        picoToDiff = Time.picosecondsToDiffTime . round
+        
+        roundRational :: Rational -> Rational
+        roundRational = fromInteger . round
+
+        fifteenMinutes :: Rational
+        fifteenMinutes = diffToPico $ minutesToDiffTime 15
+ in picoToDiff
+      ( roundRational (diffToPico diffTime / fifteenMinutes)
+      * fifteenMinutes
+      )
+
+timeWorkedToday
+  :: TimeOfDay
+  -> String
+  -> String
+timeWorkedToday now
+    = lines
+  >>> parseDays
+  >>> last
+  >>> lineItems
+  >>> map (lineItemDuration now)
+  >>> sum
+  >>> roundToNearestFifteenMinutes
+  >>> Time.timeToTimeOfDay
+  >>> ( \(TimeOfDay hours minutes _)
+     -> "I have worked "
+     ++ show hours
+     ++ " hours and "
+     ++ show minutes
+     ++ " minutes today so far.\n"
+      )
+
+
+getCurrentTimeOfDay
+  :: IO TimeOfDay
+getCurrentTimeOfDay = do
+  now <- Time.getCurrentTime
+  timeZone <- Time.getCurrentTimeZone
+  pure $ Time.localTimeOfDay $ Time.zonedTimeToLocalTime $ Time.utcToZonedTime timeZone now
+
+test
+  :: IO ()
+test = do
+  now <- getCurrentTimeOfDay
+  s <- readFile "example-timesheet.txt"
+  putStr $ timeWorkedToday now s
+
+main
+  :: IO ()
 main = do
-  putStrLn "typechecks."
+  now <- getCurrentTimeOfDay
+  interact (timeWorkedToday now)
